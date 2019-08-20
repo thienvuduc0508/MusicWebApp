a94148cbc9c84aa28d4e31c44c88e4cf653515d7
thienvuduc0508 <vuthienktb@gmail.com>
2019-08-20 08:54:52 +0700

    create user table

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = new \App\User();
+        $users->username = 'admin';
+        $users->email = 'admin@gmail.com';
+        $users->password = \Illuminate\Support\Facades\Hash::make("123456");
+        $users->save();
+    }
+}
