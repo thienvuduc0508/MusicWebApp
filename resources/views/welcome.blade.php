0755a986bf9c866a7929b368c78a85506f4f582a
lapgun <lapgun06@gmail.com>
2019-09-05 17:09:37 +0700

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,16 +67,6 @@
 
                     </div>
                 </div>
-                       <!--Controls-->
-            <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-            <!--/.Controls-->
         {{--end slide--}}
 
         {{--list song--}}
