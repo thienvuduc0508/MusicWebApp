63e36a924985616bdf9cd786eab109bd3fdcc317
lapgun <lapgun06@gmail.com>
2019-09-12 08:52:47 +0700

    create

--- resources/views/singers/create.blade.php
+++ resources/views/singers/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+    @extends('layouts.app')
 @section('content')
     <div class="container justify-content-center col-md-10">
         <form action="{{route('singer.store')}}" method="post" enctype="multipart/form-data">
